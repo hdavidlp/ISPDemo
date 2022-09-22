cb78d42dbfcec549dcf193fbbbcbecfcf7f60f0b
hdavidlp <heclop@hotmail.com>
2022-09-22 10:45:23 -0700

    ISP Example

---
+++ DemoLibrary/ReferenceBook.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary
+{
+    public class ReferenceBook: ILibraryItem
+    {
+        public string LibraryId { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }  
+        public int Pages { get; set; }
+        public int CheckOutDurationInDays { get; set; }
+        public string Borrower { get; set; }
+        public DateTime BorrowDate { get; set; }
+
+        void ILibraryItem.CheckIn()
+        {
+            throw new NotImplementedException();
+        }
+
+        void ILibraryItem.CheckOut(string borrower)
+        {
+            throw new NotImplementedException();
+        }
+
+        DateTime ILibraryItem.GetDueDate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
